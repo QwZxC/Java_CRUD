0aa9174eb5618403274075ddd963e870f018e5c9
Миша Вагин <mihail.vagin.10@mail.ru>
2023-10-26 19:58:57 +0300

    removed unused import

--- src/main/java/com/example/java_crud/repository/ProductRepository.java
+++ src/main/java/com/example/java_crud/repository/ProductRepository.java
@@ -2,7 +2,6 @@ package com.example.java_crud.repository;
 
 import com.example.java_crud.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
